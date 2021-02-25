fbb06c479655e1b1b33b4c865675d538f39f5f65
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-26 01:00:51 +0600

    login ui done

--- lib/Screens/Signup/components/body.dart
+++ lib/Screens/Signup/components/body.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_svg/svg.dart';
-import 'package:login_with_flutter/Screens/Welcome/Components/round_button.dart';
+import 'package:login_with_flutter/Screens/Login/login_screen.dart';
+import 'package:login_with_flutter/Screens/Signup/components/social_icon.dart';
+import 'package:login_with_flutter/components/already_have_an_account_acheck.dart';
+import 'package:login_with_flutter/components/rounded_button.dart';
+import 'package:login_with_flutter/components/rounded_input_field.dart';
+import 'package:login_with_flutter/components/rounded_password_field.dart';
 
 import 'background.dart';
+import 'or_divider.dart';
 
 class Body extends StatelessWidget {
-  const Body({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -18,15 +21,14 @@ class Body extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              "LOGIN",
+              "SIGNUP",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             SizedBox(height: size.height * 0.03),
             SvgPicture.asset(
-              "assets/icons/login.svg",
+              "assets/icons/signup.svg",
               height: size.height * 0.35,
             ),
-            SizedBox(height: size.height * 0.03),
             RoundedInputField(
               hintText: "Your Email",
               onChanged: (value) {},
@@ -35,22 +37,41 @@ class Body extends StatelessWidget {
               onChanged: (value) {},
             ),
             RoundedButton(
-              text: "LOGIN",
+              text: "SIGNUP",
               press: () {},
             ),
             SizedBox(height: size.height * 0.03),
             AlreadyHaveAnAccountCheck(
+              login: false,
               press: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      return SignUpScreen();
+                      return LoginScreen();
                     },
                   ),
                 );
               },
             ),
+            OrDivider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SocalIcon(
+                  iconSrc: "assets/icons/facebook.svg",
+                  press: () {},
+                ),
+                SocalIcon(
+                  iconSrc: "assets/icons/twitter.svg",
+                  press: () {},
+                ),
+                SocalIcon(
+                  iconSrc: "assets/icons/google-plus.svg",
+                  press: () {},
+                ),
+              ],
+            )
           ],
         ),
       ),
